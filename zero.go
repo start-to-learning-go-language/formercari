79ab7c544b843276f19d659c202b633ba28902f5
Takumi Sato <satotaku.kamigou@gmail.com>
2018-01-03 11:35:16 +0900

    ゼロ値の確認を追加

---
+++ zero.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main()  {
+  var b bool
+  var i int
+  var r rune
+  var f float64
+  var c complex64
+  var s string
+
+  fmt.Println("bool =    ", b)
+  fmt.Println("int =    ", i)
+  fmt.Println("rune =     " r)
+  fmt.Println("float =   ", f)
+  fmt.Println("complex = ", c)
+  fmt.Println("string =   ", s)
+}
